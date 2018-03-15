6e1c8b6b0c630a6481b0fbeede386cd7cebae744
fLaVz <juliendelvaux@neuf.fr>
2018-03-16 00:34:22 +0100

    init

---
+++ app/src/main/java/com/androidapp/Player.java
@@ -0,0 +1,19 @@
+/*package com.androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+
+    public void onClickPlayButton(View v) {
+        System.out.println("Click");
+    }
+}
+*/
