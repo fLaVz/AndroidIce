abdf8633c3da2c1c21665547d5df72342e96de7e
fLaVz <juliendelvaux@neuf.fr>
2018-03-17 01:00:34 +0100

    Ice and play button fully working + clean code

--- app/src/main/java/com/androidapp/Client.java
+++ app/src/main/java/com/androidapp/Client.java
@@ -1,27 +1,13 @@
 package com.androidapp;
 
-import android.annotation.TargetApi;
-import android.net.sip.SipAudioCall;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.os.Handler;
 
 import com.zeroc.Ice.Communicator;
-import com.zeroc.Ice.InitializationData;
-import com.zeroc.Ice.Util;
 import mp3App.*;
 
-import static mp3App.FunctionPrx.checkedCast;
-
 public class Client extends AppCompatActivity {
-
-	private boolean _destroyed = false;
-	private Handler _handler;
-	private Communicator _communicator;
-	private String _loginError;
-	private SipAudioCall.Listener _loginListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +19,7 @@ public class Client extends AppCompatActivity {
 		System.out.println("Click");
 
 		try {
-			com.zeroc.Ice.Communicator ic = com.zeroc.Ice.Util.initialize();
+			Communicator ic = com.zeroc.Ice.Util.initialize();
 			FunctionPrx function = FunctionPrx.checkedCast(ic.stringToProxy("server:tcp -h 10.0.2.2 -p 10000"));
 			if (function == null) {
 				throw new Error("Invalid proxy");
